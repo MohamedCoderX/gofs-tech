79d410eb1f9507805496c8d6fa3b6955aeb51537
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-04-01 09:40:27 +0530

    as

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://www.gofstech.com/', 
+    generateRobotsTxt: true, 
+  };
+  
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,40 @@
-
 import "./globals.css";
-
+import Head from "next/head";
 
 export const metadata = {
-  title: "GOFS Technology",
-  description: "gofs technology is a software development company that provides software development services, web development services, mobile app development services, and digital marketing services. We are a team of experienced software developers, web developers, mobile app developers, and digital marketers who are passionate about building high-quality software products and delivering exceptional services to our clients.",
+  title: "GOFS Technology - Software Development Services",
+  description:
+    "GOFS Technology is a software development company offering web development, mobile app development, and digital marketing services. Our experienced team builds high-quality software solutions for businesses worldwide.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content="software development, web development, mobile apps, digital marketing, GOFS Technology" />
+        <meta name="robots" content="index, follow" />
 
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
-      </head>
-      <body
-        className={``}
-      >
+        {/* Open Graph (Facebook & LinkedIn) */}
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content="https://yourdomain.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://yourdomain.com/og-image.jpg" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="https://yourdomain.com/og-image.jpg" />
+
+        {/* Preconnect & Font Optimization */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" />
+      </Head>
+      <body className="font-montserrat">
         {children}
       </body>
     </html>
